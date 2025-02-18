b5a8cc74f3efbfdc014ee9fb0e4b283d4766709f
Zac <zpaulson001@gmail.com>
2025-02-18 11:14:42 -0800

    create Vue app in frontend folder

---
+++ frontend/src/components/TranscriptionBox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import useTranscriptionStore from '@/stores/transcriptionStore';
+
+const transcriptionStore = useTranscriptionStore();
+</script>
+
+<template>
+  <div>
+    <span>{{ transcriptionStore.committed }}</span>
+    <span class="uncommitted">{{ transcriptionStore.uncommitted }}</span>
+  </div>
+</template>
+
+<style scoped>
+.uncommitted {
+  color: #969696;
+}
+</style>
