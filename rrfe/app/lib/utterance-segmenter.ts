2115a457ae1dba46d9793457f4476783f3724b2b
Zac <zpaulson001@gmail.com>
2025-06-10 16:32:58 -0700

    add basic utility functions for working with audio as Float32 arrays

--- rrfe/app/lib/utterance-segmenter.ts
+++ rrfe/app/lib/utterance-segmenter.ts
@@ -1,7 +1,10 @@
+import { concatenateFloat32Arrays } from './audio-utils';
+
 const SAMPLE_RATE = 16000;
 
 export default class UtteranceSegmenter {
   private audioBuffer: Float32Array = new Float32Array(0);
+  private lastSpeechLoc: number = 0;
   private containsSpeech: boolean = false;
   private speechProbThreshold: number;
   private silenceDuration: number;
@@ -17,33 +20,33 @@ export default class UtteranceSegmenter {
     this.onNewUtterance = onNewUtterance;
   }
 
-  private reset() {
-    this.audioBuffer = new Float32Array(0);
-    this.containsSpeech = false;
-  }
-
   process(audioChunk: Float32Array, speechProb: number) {
     // Create a new buffer with space for existing data plus new chunk
-    const newBuffer = new Float32Array(
-      this.audioBuffer.length + audioChunk.length
-    );
-    // Copy existing data
-    newBuffer.set(this.audioBuffer);
-    // Append new chunk
-    newBuffer.set(audioChunk, this.audioBuffer.length);
+    const newBuffer = concatenateFloat32Arrays(this.audioBuffer, audioChunk);
+
     this.audioBuffer = newBuffer;
 
     if (speechProb >= this.speechProbThreshold) {
       this.containsSpeech = true;
+      this.lastSpeechLoc = this.audioBuffer.length;
     }
 
-    if (this.audioBuffer.length > SAMPLE_RATE * this.silenceDuration) {
+    if (
+      this.audioBuffer.length - this.lastSpeechLoc >=
+      SAMPLE_RATE * this.silenceDuration
+    ) {
       console.log('Audio buffer length:', this.audioBuffer.length);
       if (this.containsSpeech) {
         console.log('New utterance detected');
         this.onNewUtterance(this.audioBuffer);
+
+        // Keep only the audio data from the last speech location onwards
+        this.audioBuffer = this.audioBuffer.slice(this.lastSpeechLoc);
+      } else {
+        this.audioBuffer = this.audioBuffer.slice(audioChunk.length);
       }
-      this.reset();
+      this.containsSpeech = false;
+      this.lastSpeechLoc = 0;
     }
   }
 }
