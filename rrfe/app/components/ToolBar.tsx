1ef2e8c17b6322438537d5074645a963c986d12a
Zac <zpaulson001@gmail.com>
2025-06-10 16:35:53 -0700

    display transcriptions

--- rrfe/app/components/ToolBar.tsx
+++ rrfe/app/components/ToolBar.tsx
@@ -10,59 +10,47 @@ import {
 } from 'lucide-react';
 import { useState } from 'react';
 
-import { LanguageSelect } from './LanguageSelect';
 import { TranscriptionSettingsMenu } from './TranscriptionSettingsMenu';
 import useTranscription from '../hooks/useTranscription';
+import useRemoteTranscription from '~/hooks/useRemoteTranscription';
 export function Toolbar() {
-  const [selectedLanguages, setSelectedLanguages] = useState<string>('');
   const [selectedDevice, setSelectedDevice] = useState<string>('');
-  const { streamState, toggleStreaming, isSpeaking } =
-    useTranscription(selectedDevice);
-
-  const languageList = [
-    {
-      label: 'Chinese',
-      value: 'zh',
-    },
-    {
-      label: 'Russian',
-      value: 'ru',
-    },
-    {
-      label: 'Spanish',
-      value: 'es',
-    },
-  ];
+  const { streamState, toggleStreaming, isSpeaking, transcription } =
+    useRemoteTranscription(selectedDevice);
 
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 flex items-center justify-center border border-border px-4 py-2 gap-4 rounded-lg bg-background shadow-lg">
-      <TranscriptionSettingsMenu />
-      <DeviceSelect
-        selectedDevice={selectedDevice}
-        setSelectedDevice={setSelectedDevice}
-      />
-      <LanguageSelect
-        options={languageList}
-        value={selectedLanguages}
-        onChange={setSelectedLanguages}
-      />
+    <>
+      <div className="fixed bottom-4 left-1/2 -translate-x-1/2 flex items-center justify-center border border-border px-4 py-2 gap-4 rounded-lg bg-background shadow-lg">
+        <TranscriptionSettingsMenu />
+        <DeviceSelect
+          selectedDevice={selectedDevice}
+          setSelectedDevice={setSelectedDevice}
+        />
 
-      {isSpeaking ? <Speech /> : <AudioWaveform />}
+        {isSpeaking ? <Speech /> : <AudioWaveform />}
 
-      <Button
-        variant={streamState === 'streaming' ? 'destructive' : 'outline'}
-        size="icon"
-        onClick={toggleStreaming}
-        disabled={streamState === 'connecting'}
-      >
-        {streamState === 'connecting' ? (
-          <LoaderCircle className="animate-spin" />
-        ) : streamState === 'streaming' ? (
-          <StopCircle />
-        ) : (
-          <Circle fill="var(--destructive)" stroke="transparent" />
-        )}
-      </Button>
-    </div>
+        <Button
+          variant={streamState === 'streaming' ? 'destructive' : 'outline'}
+          size="icon"
+          onClick={toggleStreaming}
+          disabled={streamState === 'connecting'}
+        >
+          {streamState === 'connecting' ? (
+            <LoaderCircle className="animate-spin" />
+          ) : streamState === 'streaming' ? (
+            <StopCircle />
+          ) : (
+            <Circle fill="var(--destructive)" stroke="transparent" />
+          )}
+        </Button>
+      </div>
+      <div>
+        {transcription.map((t) => (
+          <p key={t.id}>
+            {t.text} ({t.inferenceTime.toFixed(2)}s)
+          </p>
+        ))}
+      </div>
+    </>
   );
 }
