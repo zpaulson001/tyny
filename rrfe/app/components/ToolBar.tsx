edb5eab90dc4c41172962a9186e39b8a47723162
Zac <zpaulson001@gmail.com>
2025-06-13 15:28:23 -0700

    move state up into home component

--- rrfe/app/components/ToolBar.tsx
+++ rrfe/app/components/ToolBar.tsx
@@ -2,91 +2,148 @@ import { Button } from './ui/button';
 import { DeviceSelect } from './DeviceSelect';
 
 import {
-  AudioWaveform,
   Circle,
   LoaderCircle,
   Speech,
-  StopCircle,
+  Square,
+  FileMusic,
+  Mic,
+  Play,
+  Ellipsis,
 } from 'lucide-react';
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { TranscriptionSettingsMenu } from './TranscriptionSettingsMenu';
 import useLocalTranscription from '../hooks/useLocalTranscription';
-import { Switch } from './ui/switch';
-import { Label } from './ui/label';
 import { Input } from './ui/input';
+import { LanguageSelect } from './LanguageSelect';
+import { ToggleGroup, ToggleGroupItem } from './ui/toggle-group';
 
-export function Toolbar() {
-  const [selectedDevice, setSelectedDevice] = useState<string>('');
-  const [fileBuffer, setFileBuffer] = useState<ArrayBuffer | undefined>(
-    undefined
+function PlayStopButton({
+  streamState,
+  mode,
+  onClick,
+}: {
+  streamState: string;
+  mode: 'file' | 'mic';
+  onClick: () => void;
+}) {
+  if (mode === 'file') {
+    return (
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={onClick}
+        disabled={streamState === 'connecting'}
+      >
+        {streamState === 'connecting' ? (
+          <LoaderCircle className="animate-spin" />
+        ) : streamState === 'streaming' ? (
+          <Square />
+        ) : (
+          <Play />
+        )}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      variant={streamState === 'streaming' ? 'destructive' : 'outline'}
+      size="icon"
+      onClick={onClick}
+      disabled={streamState === 'connecting'}
+    >
+      {streamState === 'connecting' ? (
+        <LoaderCircle className="animate-spin" />
+      ) : streamState === 'streaming' ? (
+        <Square />
+      ) : (
+        <Circle fill="var(--destructive)" stroke="transparent" />
+      )}
+    </Button>
   );
-  const [isTestMode, setIsTestMode] = useState(false);
-  const { streamState, toggleStreaming, isSpeaking, mostRecentTranscription } =
-    useLocalTranscription({
-      deviceId: selectedDevice,
-      file: fileBuffer,
-    });
+}
 
-  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
+interface ToolbarProps {
+  streamState: string;
+  toggleStreaming: () => void;
+  selectedDevice: string;
+  setSelectedDevice: (device: string) => void;
+  selectedLanguage: string;
+  setSelectedLanguage: (language: string) => void;
+  isSpeaking: boolean;
+  onFileInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-      reader.onload = async (e) => {
-        setFileBuffer(e.target?.result as ArrayBuffer);
-      };
+export function Toolbar({
+  selectedDevice,
+  setSelectedDevice,
+  selectedLanguage,
+  setSelectedLanguage,
+  isSpeaking,
+  onFileInputChange,
+  streamState,
+  toggleStreaming,
+}: ToolbarProps) {
+  const [mode, setMode] = useState<'file' | 'mic'>('mic');
 
-      reader.onerror = (err) => {
-        console.error('FileReader error:', err);
-      };
-
-      reader.readAsArrayBuffer(file);
+  const handleModeChange = (value: string) => {
+    if (value !== 'file' && value !== 'mic') {
+      throw new Error('Invalid mode. Must be either "file" or "mic".');
     }
+    setMode(value);
   };
 
   return (
-    <>
-      <div className="fixed bottom-4 left-1/2 -translate-x-1/2 flex items-center justify-center border border-border px-4 py-2 gap-4 rounded-lg bg-background shadow-lg">
-        <TranscriptionSettingsMenu />
-        <div className="flex items-center space-x-2">
-          <Switch
-            id="test-mode"
-            checked={isTestMode}
-            onCheckedChange={setIsTestMode}
-          />
-          <Label htmlFor="test-mode">Test Mode</Label>
-        </div>
-        {isTestMode && (
-          <div className="flex items-center space-x-2 w-32">
-            <Input type="file" accept="audio/*" onChange={handleFileChange} />
-          </div>
-        )}
+    <div className=" w-fit-content flex items-center justify-center border border-border px-4 py-4 gap-4 rounded-lg bg-background shadow-lg">
+      <div className="flex items-center space-x-2">
+        <ToggleGroup
+          type="single"
+          variant="outline"
+          value={mode}
+          onValueChange={handleModeChange}
+          className="flex items-center"
+        >
+          <ToggleGroupItem value="file" aria-label="File Mode">
+            <FileMusic />
+            File Mode
+          </ToggleGroupItem>
+          <ToggleGroupItem value="mic" aria-label="Mic Mode">
+            <Mic />
+            Mic Mode
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+      {mode === 'file' ? (
+        <Input
+          type="file"
+          accept="audio/*"
+          onChange={onFileInputChange}
+          className="min-w-48 flex-1"
+        />
+      ) : (
         <DeviceSelect
           selectedDevice={selectedDevice}
           setSelectedDevice={setSelectedDevice}
         />
+      )}
 
-        {isSpeaking ? <Speech /> : <AudioWaveform />}
+      <LanguageSelect value={selectedLanguage} onChange={setSelectedLanguage} />
 
-        <Button
-          variant={streamState === 'streaming' ? 'destructive' : 'outline'}
-          size="icon"
-          onClick={toggleStreaming}
-          disabled={streamState === 'connecting'}
-        >
-          {streamState === 'connecting' ? (
-            <LoaderCircle className="animate-spin" />
-          ) : streamState === 'streaming' ? (
-            <StopCircle />
-          ) : (
-            <Circle fill="var(--destructive)" stroke="transparent" />
-          )}
-        </Button>
-      </div>
-      <div>
-        <p>{mostRecentTranscription}</p>
-      </div>
-    </>
+      {isSpeaking ? (
+        <div className="flex items-center space-x-2">
+          <Speech />
+        </div>
+      ) : (
+        <div className="flex items-center space-x-2">
+          <Ellipsis />
+        </div>
+      )}
+      <PlayStopButton
+        streamState={streamState}
+        mode={mode}
+        onClick={toggleStreaming}
+      />
+    </div>
   );
 }
--- rrfe/app/routes/home.tsx
+++ rrfe/app/routes/home.tsx
@@ -1,14 +1,67 @@
 import { Toolbar } from '~/components/ToolBar';
 import type { Route } from './+types/home';
+import useLocalTranscription from '~/hooks/useLocalTranscription';
+import { useState } from 'react';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Tyny | Real-time translation' }];
 }
 
 export default function Home() {
+  const [selectedDevice, setSelectedDevice] = useState<string>('');
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('spa_Latn');
+  const [fileBuffer, setFileBuffer] = useState<ArrayBuffer | undefined>(
+    undefined
+  );
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = async (e) => {
+        setFileBuffer(e.target?.result as ArrayBuffer);
+      };
+
+      reader.onerror = (err) => {
+        console.error('FileReader error:', err);
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const { streamState, toggleStreaming, isSpeaking, transcription } =
+    useLocalTranscription({
+      input: {
+        deviceId: selectedDevice,
+        file: fileBuffer,
+      },
+      targetLanguage: selectedLanguage,
+    });
+
   return (
-    <div className="h-full w-full">
-      <Toolbar />
+    <div className="h-screen w-full border border-green-500">
+      <div className="h-full w-full max-w-5xl mx-auto grid grid-rows-[1fr_auto] gap-2 border border-red-500 p-4">
+        <div className="w-full grid gap-2 overflow-y-auto">
+          {transcription.map((t) => (
+            <div key={t.id} className="grid grid-cols-2 gap-2">
+              <p>{t.text}</p>
+              <p>{t.translation}</p>
+            </div>
+          ))}
+        </div>
+        <Toolbar
+          selectedDevice={selectedDevice}
+          setSelectedDevice={setSelectedDevice}
+          selectedLanguage={selectedLanguage}
+          setSelectedLanguage={setSelectedLanguage}
+          isSpeaking={isSpeaking}
+          onFileInputChange={handleFileChange}
+          streamState={streamState}
+          toggleStreaming={toggleStreaming}
+        />
+      </div>
     </div>
   );
 }
