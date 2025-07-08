a37882f0029c9b507217cc559a318b176c473ae8
Zac <zpaulson001@gmail.com>
2025-07-08 11:15:56 -0700

    remove unused env variables, make sure deepl and modal are required

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,21 +1,14 @@
 import logging
-from typing import Literal
 
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    translation_service: Literal["azure", "nllb"] = "azure"
-    azure_translate_api_key: str = ""
-    azure_translate_url: str = ""
-    azure_region: str = ""
-    gcp_translate_api_key: str = ""
-    gcp_translate_url: str = ""
-    deepl_api_key: str = ""
-    deepl_url: str = ""
-    modal_key: str = ""
-    modal_secret: str = ""
-    modal_url: str = ""
+    deepl_api_key: str
+    deepl_url: str
+    modal_key: str
+    modal_secret: str
+    modal_url: str
 
 
 logger = logging.getLogger("uvicorn")
