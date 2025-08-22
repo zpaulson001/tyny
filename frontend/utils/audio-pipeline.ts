d662dd4a80d033e1cc023940abf08f6b009df800
Zac <zpaulson001@gmail.com>
2025-08-22 15:59:53 -0700

    perform downsampling of audio from mic in the outlet worklet to work around Firefox's lack of support for automatic resamling from input node

--- frontend/utils/audio-pipeline.ts
+++ frontend/utils/audio-pipeline.ts
@@ -1,55 +1,3 @@
-// export class AudioPipeline {
-//   private mediaRecorder: MediaRecorder | null = null;
-//   private audioContext: AudioContext | null = null;
-//   private inputStream: MediaStream | null = null;
-//   private onChunk: ((chunk: Float32Array) => void | Promise<void>) | null =
-//     null;
-//   constructor(
-//     inputStream: MediaStream,
-//     onChunk?: (chunk: Float32Array) => void | Promise<void>
-//   ) {
-//     this.inputStream = inputStream;
-//     this.onChunk = onChunk || null;
-//   }
-
-//   async start(): Promise<void> {
-//     console.log('Starting audio pipeline');
-//     if (!this.inputStream) {
-//       throw new Error('Input stream is required');
-//     }
-//     this.audioContext = new AudioContext({
-//       sampleRate: 16000,
-//     });
-//     this.mediaRecorder = new MediaRecorder(this.inputStream);
-//     this.mediaRecorder.ondataavailable = (event) => {
-//       if (event.data.size > 0 && this.mediaRecorder) {
-//         const blob = new Blob([event.data], {
-//           type: this.mediaRecorder.mimeType,
-//         });
-//         const fileReader = new FileReader();
-//         fileReader.onloadend = async () => {
-//           const arrayBuffer = fileReader.result as ArrayBuffer;
-//           const audioBuffer =
-//             await this.audioContext?.decodeAudioData(arrayBuffer);
-//           const audio = audioBuffer?.getChannelData(0);
-//           if (audio) {
-//             console.log('Audio in seconds', audio.length / 16000);
-//             this.onChunk?.(audio);
-//           }
-//         };
-//         fileReader.readAsArrayBuffer(blob);
-//       }
-//     };
-//   }
-
-//   async stop(): Promise<void> {
-//     if (this.mediaRecorder) {
-//       this.mediaRecorder.stop();
-//       this.mediaRecorder = null;
-//     }
-//   }
-// }
-
 export class AudioPipeline {
   private audioContext: AudioContext | null = null;
   private inputSource: MediaStream | ArrayBuffer;
@@ -61,6 +9,7 @@ export class AudioPipeline {
   private gainNode: GainNode | null = null;
   private compressorNode: DynamicsCompressorNode | null = null;
   private destinationNode: MediaStreamAudioDestinationNode | null = null;
+  private filterNode: BiquadFilterNode | null = null;
 
   constructor(
     inputSource: MediaStream | ArrayBuffer,
@@ -71,47 +20,19 @@ export class AudioPipeline {
   }
 
   async start(): Promise<void> {
-    this.audioContext = new AudioContext({
-      sampleRate: 16000,
-    });
+    const isMicInput = this.inputSource instanceof MediaStream;
+    const audioContextOptions = isMicInput ? undefined : { sampleRate: 16000 };
+
+    this.audioContext = new AudioContext(audioContextOptions);
 
     await this.audioContext.audioWorklet.addModule('/worklets/outlet.js');
 
-    if (this.inputSource instanceof MediaStream) {
-      this.inputNode = this.audioContext.createMediaStreamSource(
-        this.inputSource
-      );
-    } else {
-      const audioBuffer = await this.audioContext.decodeAudioData(
-        this.inputSource
-      );
-      this.inputNode = this.audioContext.createBufferSource();
-      this.inputNode.buffer = audioBuffer;
-    }
+    this.inputNode = await this.createInputNode(isMicInput ? 'mic' : 'file');
 
     this.destinationNode = this.audioContext.createMediaStreamDestination();
-
-    this.compressorNode = this.audioContext.createDynamicsCompressor();
-    this.compressorNode.threshold.value = -20; // Start compressing at -20 dB
-    this.compressorNode.knee.value = 10; // Soft knee for a smoother transition
-    this.compressorNode.ratio.value = 3; // 3:1 compression ratio
-    this.compressorNode.attack.value = 0.01; // Fast attack (10ms) to catch sudden peaks
-    this.compressorNode.release.value = 0.15; // Relatively fast release (150ms)
-
-    this.gainNode = this.audioContext.createGain();
-    this.gainNode.gain.value = 1.2;
-
-    this.outletNode = new AudioWorkletNode(
-      this.audioContext,
-      'outlet-worklet',
-      {
-        numberOfInputs: 1,
-        numberOfOutputs: 1,
-        processorOptions: {
-          outputSize: 512,
-        },
-      }
-    );
+    this.compressorNode = this.createCompressorNode();
+    this.gainNode = this.createGainNode();
+    this.outletNode = this.createOutletNode(isMicInput);
 
     if (this.onChunk) {
       this.outletNode.port.onmessage = async (event) => {
@@ -128,31 +49,118 @@ export class AudioPipeline {
       };
     }
 
-    this.inputNode.connect(this.compressorNode);
-    this.compressorNode.connect(this.gainNode);
-    this.gainNode.connect(this.outletNode);
-    this.outletNode.connect(this.destinationNode);
+    if (isMicInput) {
+      this.filterNode = this.createFilterNode();
+      this.inputNode.connect(this.compressorNode);
+      this.compressorNode.connect(this.filterNode);
+      this.filterNode.connect(this.outletNode);
+      this.gainNode.connect(this.outletNode);
+      this.outletNode.connect(this.destinationNode);
+    } else {
+      this.inputNode.connect(this.outletNode);
+      this.outletNode.connect(this.destinationNode);
+    }
 
     if (this.inputNode instanceof AudioBufferSourceNode) {
       this.inputNode.start();
     }
   }
 
+  private async createInputNode(
+    sourceType: 'mic' | 'file'
+  ): Promise<MediaStreamAudioSourceNode | AudioBufferSourceNode> {
+    if (this.audioContext) {
+      if (sourceType === 'mic') {
+        return this.audioContext.createMediaStreamSource(
+          this.inputSource as MediaStream
+        );
+      }
+      const audioBuffer = await this.audioContext.decodeAudioData(
+        this.inputSource as ArrayBuffer
+      );
+      this.inputNode = this.audioContext.createBufferSource();
+      this.inputNode.buffer = audioBuffer;
+      return this.inputNode;
+    }
+    throw new Error('Audio context is not initialized');
+  }
+
+  private createOutletNode(
+    downsample: boolean = false,
+    downsampleFactor?: number
+  ): AudioWorkletNode {
+    if (this.audioContext) {
+      return new AudioWorkletNode(this.audioContext, 'outlet-worklet', {
+        numberOfInputs: 1,
+        numberOfOutputs: 1,
+        processorOptions: {
+          outputSize: 512,
+          downsample,
+          downsampleFactor,
+        },
+      });
+    }
+    throw new Error('Audio context is not initialized');
+  }
+
+  private createCompressorNode(): DynamicsCompressorNode {
+    if (this.audioContext) {
+      const compressorNode = this.audioContext.createDynamicsCompressor();
+      compressorNode.threshold.value = -20; // Start compressing at -20 dB
+      compressorNode.knee.value = 10; // Soft knee for a smoother transition
+      compressorNode.ratio.value = 3; // 3:1 compression ratio
+      compressorNode.attack.value = 0.01; // Fast attack (10ms) to catch sudden peaks
+      compressorNode.release.value = 0.15; // Relatively fast release (150ms)
+      return compressorNode;
+    }
+    throw new Error('Audio context is not initialized');
+  }
+  private createFilterNode(): BiquadFilterNode {
+    if (this.audioContext) {
+      const lowPassFilter = this.audioContext.createBiquadFilter();
+
+      // Set the filter type to lowpass
+      lowPassFilter.type = 'lowpass';
+
+      // Set the cutoff frequency. A value like 7500-7800 Hz is a good choice,
+      // leaving some headroom below the Nyquist frequency of 8000 Hz.
+      const targetNyquistFrequency = 16000 / 2;
+      lowPassFilter.frequency.setValueAtTime(
+        targetNyquistFrequency * 0.95,
+        this.audioContext.currentTime
+      );
+
+      // Optionally, you can set the Q-factor to shape the filter's curve.
+      // A Q of 1.0 is a good starting point for a clean, non-resonant filter.
+      lowPassFilter.Q.setValueAtTime(1.0, this.audioContext.currentTime);
+      return lowPassFilter;
+    }
+    throw new Error('Audio context is not initialized');
+  }
+
+  private createGainNode(): GainNode {
+    if (this.audioContext) {
+      const gainNode = this.audioContext.createGain();
+      gainNode.gain.value = 1.2;
+      return gainNode;
+    }
+    throw new Error('Audio context is not initialized');
+  }
+
   async stop(): Promise<void> {
     if (this.inputNode instanceof AudioBufferSourceNode) {
       this.inputNode.stop();
-    }
-    if (this.outletNode) {
-      this.outletNode.disconnect();
-      this.outletNode = null;
-    }
-    if (this.inputNode) {
-      this.inputNode.disconnect();
-      this.inputNode = null;
     }
     if (this.audioContext) {
       await this.audioContext.close();
       this.audioContext = null;
     }
+    // Reset all node references
+    this.inputNode = null;
+    this.outletNode = null;
+    this.filterNode = null;
+    this.compressorNode = null;
+    this.gainNode = null;
+    this.destinationNode = null;
   }
 }
